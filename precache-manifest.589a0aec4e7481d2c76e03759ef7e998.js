b06b7f34fafc3113aa8ad67ebb69b3eddc84245a
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-04-13 14:16:25 +0300

    Updates

---
+++ precache-manifest.589a0aec4e7481d2c76e03759ef7e998.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f456469d1941829319b92d72dcbb07f4",
+    "url": "/25-5-clock-pomodoro/index.html"
+  },
+  {
+    "revision": "7852a87e2297530ec5b9",
+    "url": "/25-5-clock-pomodoro/static/css/2.0b8fbf56.chunk.css"
+  },
+  {
+    "revision": "451bcc1ac66f02258057",
+    "url": "/25-5-clock-pomodoro/static/css/main.d02efd7d.chunk.css"
+  },
+  {
+    "revision": "7852a87e2297530ec5b9",
+    "url": "/25-5-clock-pomodoro/static/js/2.952563e5.chunk.js"
+  },
+  {
+    "revision": "451bcc1ac66f02258057",
+    "url": "/25-5-clock-pomodoro/static/js/main.85b0d8d9.chunk.js"
+  },
+  {
+    "revision": "a6653aa55c1aa35596ff",
+    "url": "/25-5-clock-pomodoro/static/js/runtime~main.3d927c31.js"
+  },
+  {
+    "revision": "342c532d381f22ed18d983ae1b3b8c96",
+    "url": "/25-5-clock-pomodoro/static/media/beep.342c532d.mp3"
+  }
+]);
